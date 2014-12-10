7d3a48bc0e875be4b168434b1d93c3996a518ef9
Kaito Kinjo <kaitokinjo@gmail.com>
2014-12-10 23:52:07 +0900

    add googlemap

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var fs = require('fs');
 var app = require('http').createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end(fs.readFileSync('index.html'));
-
 }).listen(3000);
 
 
@@ -15,8 +14,9 @@ io.sockets.on('connection', function(socket) {
 
   socket.on('msg', function(data) {
     var keyword = data;
-    var option = {'track': keyword, 'language': 'ja', 'include_entities': 'true'};
-    console.log(keyword+'を含むツイートを取得しま〜す♪');
+    //var option = {'track': keyword, 'language': 'ja', 'include_entities': 'true'};
+    var option = {'track': keyword, 'include_entities': 'true'};
+    console.log(keyword+'を含むツイートを取得します。');
     twit.stream('statuses/filter', option, function(stream) {
       stream.on('data', function (data) {
         io.sockets.emit('data', data);
