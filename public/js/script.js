fb458c9ead0478e2309ef61bcf4fb77a679d429c
Kaito Kinjo <kaitokinjo@gmail.com>
2015-01-12 16:08:25 +0900

    clean

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
   $(function() {
 
   	var socket = io.connect();
+
   	$('form').submit(function() {
   		socket.emit('msg', $('input').val());
   		return false;
@@ -14,16 +15,8 @@
         var longitude = geo[1];//経度
         setMapping(latitude,longitude,data,image_url);
       }
-
     });
-  	$('#stop').click(function(){
-  		socket.emit('stop', $('input').val());
-  		return false;
-  	})
-  });
 
-  var map;
-  $(document).ready( function () {
   	var myLatlng = new google.maps.LatLng('28.37', '180.76');
   	var myOptions = {
   		zoom: 2,
@@ -32,43 +25,46 @@
   	};
   	var canvas = $('#map_canvas')[0];
   	map = new google.maps.Map(canvas, myOptions);
-    var styleOptions = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"},{"weight":"0.20"},{"lightness":"28"},{"saturation":"23"},{"visibility":"off"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#494949"},{"lightness":13},{"visibility":"off"}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#08304b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},{"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},{"featureType":"transit","elementType":"all","stylers":[{"color":"#146474"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#021019"}]}];
-    var styledMapOptions = { name: 'tweetflock' }
-      var sampleType = new google.maps.StyledMapType(styleOptions, styledMapOptions);
-      map.mapTypes.set('tweetflock', sampleType);
-      map.setMapTypeId('tweetflock');
+    //var styleOptions = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"},{"weight":"0.20"},{"lightness":"28"},{"saturation":"23"},{"visibility":"off"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#494949"},{"lightness":13},{"visibility":"off"}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#08304b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},{"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},{"featureType":"transit","elementType":"all","stylers":[{"color":"#146474"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#021019"}]}];
+    //var styledMapOptions = { name: 'tweetflock' }
+    //var sampleType = new google.maps.StyledMapType(styleOptions, styledMapOptions);
+    //map.mapTypes.set('tweetflock', sampleType);
+    //map.setMapTypeId('tweetflock');
   });
-
+    var map;
   function setMapping(latitude,longitude,data,image_url) {
-  var myLatlng = new google.maps.LatLng(latitude,longitude); //経度,緯度
-  var marker = new google.maps.Marker({
-  	position:myLatlng,
-  	animation:google.maps.Animation.DROP
-  });
 
-  var contentTweet = '<div style="width:200px;height:150px;" class="map_text">' + '<dl><dt><a href="http://twitter.com/'+ data.user['screen_name'] + '" target="_blank">' + 
-  '<img src="' + data.user['profile_image_url'] + '" target="_blank">' +
-  '</a></dt>' + 
-  '<dt>' + data.text + '</dt>' +
-  '</dl>' + '</div>';
+    var myLatlng = new google.maps.LatLng(latitude,longitude); //経度,緯度
+    var marker = new google.maps.Marker({
+      position:myLatlng,
+      animation:google.maps.Animation.DROP
+    });
 
-  var infowindow = new google.maps.InfoWindow({
-    content: contentTweet,
-    disableAutoPan: false
-  });
-  google.maps.event.addListener(marker, 'click', function() {
-    if (isInfoWindowOpen(infowindow))
-    {
-      infowindow.close(map,marker);
-    }else{
-      infowindow.open(map,marker);
+    var contentTweet = '<div style="width:200px;height:150px;" class="map_text">' + 
+    '<dl><dt><a href="http://twitter.com/'+ data.user['screen_name'] + '" target="_blank">' + 
+    '<img src="' + data.user['profile_image_url'] + '" target="_blank">' + '</a></dt>' + 
+    '<dt>' + data.text + '</dt>' + '</dl>' + '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+      content: contentTweet,
+      disableAutoPan: false
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+
+      if (isInfoWindowOpen(infowindow))
+      {
+        infowindow.close(map,marker);
+      }else{
+        infowindow.open(map,marker);
+      }
+
+    });
+
+    function isInfoWindowOpen(infoWindow){
+      var map = infoWindow.getMap();
+      return (map !== null && typeof map !== "undefined");
     }
-  });
 
-  function isInfoWindowOpen(infoWindow){
-    var map = infoWindow.getMap();
-    return (map !== null && typeof map !== "undefined");
+    marker.setMap(map);
   }
-
-  marker.setMap(map);
-}
