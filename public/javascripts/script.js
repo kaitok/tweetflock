f95a617487332a1c6538b45741b3c958f17bc81f
Kaito Kinjo <kaitokinjo@gmail.com>
2015-02-09 22:08:55 +0900

    add procfile

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -47,12 +47,9 @@
               mapTypeControl: true,
               mapTypeControlOptions: {
                   style: google.maps.MapTypeControlStyle.DEFAULT,
-                  mapTypeIds: [
-                      google.maps.MapTypeId.ROADMAP,
-                      google.maps.MapTypeId.SATELLITE,
-                  ],
                   position: google.maps.ControlPosition.BOTTOM_LEFT
-              }
+              },
+              mapTypeId: google.maps.MapTypeId.HYBRID
           };
           var canvas = $('#map_canvas')[0];
           map = new google.maps.Map(canvas, myOptions);
@@ -77,14 +74,44 @@
 
       $('#tweet_box').prepend(contentTweet.body);
 
+      var infowindow = new google.maps.InfoWindow({
+          content: contentTweet.body,
+          disableAutoPan: false
+      });
+
       $('.tweet_window')[0].onclick = function() {
-          google.maps.event.trigger(marker, 'click');
+          google.maps.event.trigger(marker, 'sidebarclick');
       };
 
+      google.maps.event.addListener(marker, 'sidebarclick', function() {
+
+          if (isInfoWindowOpen(infowindow)) {
+              infowindow.close(map, marker);
+          } else {
+              if (map.zoom <= 18) {
+                  map.setZoom(18);
+                  map.setCenter(marker.getPosition());
+              }
+              infowindow.open(map, marker);
+          }
+
+          function isInfoWindowOpen(infoWindow) {
+              var map = infoWindow.getMap();
+              return (map !== null && typeof map !== "undefined");
+          }
+      });
+
       google.maps.event.addListener(marker, 'click', function() {
-          if (map.zoom <= 18) {
-              map.setZoom(18);
-              map.setCenter(marker.getPosition());
+
+          if (isInfoWindowOpen(infowindow)) {
+              infowindow.close(map, marker);
+          } else {
+              infowindow.open(map, marker);
+          }
+
+          function isInfoWindowOpen(infoWindow) {
+              var map = infoWindow.getMap();
+              return (map !== null && typeof map !== "undefined");
           }
       });
 
